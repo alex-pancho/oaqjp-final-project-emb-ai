47aac3cfc0d3f998dab72d701f23d3ab78ee05fb
Oleksandr Panchenko <pan.pan.4it@gmail.com>
2026-05-10 08:34:19 +0300

    add tests

--- emotion_detection/emotion_detection.py
+++ emotion_detection/emotion_detection.py
@@ -7,22 +7,18 @@ def emotion_detector(text_to_analyse):
     dengan penanganan error koneksi.
     """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    myobj = {"raw_document": {"text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
     try:
-        response = requests.post(url, json = myobj, headers=header, timeout=5)
-    except (requests.exceptions.RequestException, requests.exceptions.ConnectionError):
-        return {
-            'anger': None, 'disgust': None, 'fear': None, 'joy': None,
-            'sadness': None, 'dominant_emotion': None
-        }
+        response = requests.post(url, json = myobj, headers=header, timeout=7)
+    except (requests.exceptions.RequestException, requests.exceptions.ConnectionError) as e:
+        print(e)
+        return {}
 
     if response.status_code == 400:
-        return {
-            'anger': None, 'disgust': None, 'fear': None, 'joy': None,
-            'sadness': None, 'dominant_emotion': None
-        }
+        print("status error")
+        return {}
 
     formatted_response = json.loads(response.text)
 
@@ -38,3 +34,18 @@ def emotion_detector(text_to_analyse):
     emotion_predictions['dominant_emotion'] = dominant_emotion
 
     return emotion_predictions
+
+
+if __name__ == "__main__":
+    emo_dict = dict(
+    joy = "Great to meet @BrightonChoir @MJParanzino tonight. What a lively and enthusiastic bunch! Look forward to working with you again!",
+    fear = "She chuckles, shaking her head. 'No...I just have a really vivid imagination, I guess. It happens when you meet someone",
+    anger = "Losing the will 2 live with @virginmedia business bb gone down on hold for 23 minutes &amp; whoever picked up cut me off",
+    sadness = "it's still not sunk in that im seeing joe next month, im so grateful and excited shit",
+    joy_2 = "Sometimes I watch shitty tv to reinforce never giving up cuz if something is that fucking awful on tv, I still stand a chance.",
+    fear_2 = "#Everything you've ever wanted awaits you on the other side of #fear . #IfIWasTheOppositeSex #AskAMan",
+    anger_2 = "@SkyUK what a joke!! Cut our internet off early 'by mistake' and then don't reinstate it when we no longer need an engineer",
+    )
+    for emo, text in emo_dict.items():
+        emotion_predictions = emotion_detector(text)
+        print(emo, emotion_predictions)
